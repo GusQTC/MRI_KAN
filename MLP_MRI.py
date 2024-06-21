729646a45fa13574310e74848520515a7977faaf
GusQTC <gustavoqt@usp.br>
2024-06-21 02:48:09 -0300

    final tests

--- MLP_MRI.py
+++ MLP_MRI.py
@@ -12,19 +12,33 @@ import torchvision.transforms as transforms
 import cv2  # OpenCV for image processing
 from sklearn.preprocessing import StandardScaler
 
-
-image_pixels = 128
-neurons = 8
+dataset_path = 'new_dataset'
+image_pixels = 512
+neurons = 20
 num_samples = 1020
-input_size_pca =  image_pixels * image_pixels  # Image is 3 channels (RGB) and 512x512 image size #TODO change input for reducing size
-learning_rate = 0.05
-num_epochs = 25
-n_components_pca = 0.96
-batch_size = 96 # increasing doesnt improve accuracy,  it increases the time to train
-#image is 512 by 512 rgb
-num_classes = 2 #TODO change to 4 classes
-input_size = 96
+input_size_pca =  image_pixels * image_pixels
+learning_rate = 0.8
+num_epochs = 100
+n_components_pca = 0.90
+batch_size = 128
+num_classes = 2
+input_size = 20
 
+#Test accuracy: 0.8758169934640523
+
+#dataset_path = 'hard_dataset'
+
+#image_pixels = 512
+#neurons = 96
+#num_samples = 1020
+#input_size_pca =image_pixels * image_pixels
+#learning_rate = 0.5
+#num_epochs = 100
+#n_components_pca = 0.90
+#batch_size = 128
+#num_classes = 4 
+#input_size = 96
+#Test accuracy: 0.8743718592964824
 
 def contrast_normalization(image_np):
     "Normalization for the contrast of the image, to improve the quality of the image for the model, as the black background can affect the performance"
@@ -65,9 +79,9 @@ def apply_pca_float(image_tensor, n_components=n_components_pca, input_nn=input_
     "Applying pca to preserve a certain amount of variance, and then truncating or padding the result to have the desired number of components"
     # Convert the PyTorch tensor to a NumPy array
     image_np = image_tensor.numpy()
-    
+    print(image_np)
     # Normalize the image to have pixel values between 0 and 1
-    image_np = image_np / 255.0
+    image_np = image_np // 255.0
     
     # Apply contrast normalization
     image_np = contrast_normalization(image_np)
@@ -87,18 +101,18 @@ def apply_pca_float(image_tensor, n_components=n_components_pca, input_nn=input_
     pca_result = pca.fit_transform(images_np_flattened)
     
         # If the PCA result does not have the desired shape, pad or truncate
-    if pca_result.shape[1] < input_nn:
-        # Pad the PCA result to have the desired number of components
-        pca_result_padded = np.pad(pca_result, ((0, 0), (0, input_nn - pca_result.shape[1])), 'constant', constant_values=0)
-        transformed_image_np = pca_result_padded
-    elif pca_result.shape[1] > input_nn:
-        # Truncate the PCA result to have the desired number of components
-        transformed_image_np = pca_result[:, :input_nn]
+  # Adjust the PCA result to match the desired input size for the neural network
+    if pca_result.shape[1] != input_nn:
+        # Adjust the shape by either padding with zeros or truncating
+        pca_result_adjusted = np.zeros((pca_result.shape[0], input_nn))
+        min_cols = min(pca_result.shape[1], input_nn)
+        pca_result_adjusted[:, :min_cols] = pca_result[:, :min_cols]
+        transformed_image_np = pca_result_adjusted
     else:
         transformed_image_np = pca_result
     
     # Convert the NumPy array back to a PyTorch tensor
-    transformed_image_tensor = torch.from_numpy(transformed_image_np)
+    transformed_image_tensor = torch.from_numpy(transformed_image_np).float()
     return transformed_image_tensor
 
 # Define the transform to convert image data to tensors
@@ -106,13 +120,22 @@ transform = transforms.Compose([
     transforms.Resize((image_pixels, image_pixels)),  # Resize images for faster computation
     transforms.ToTensor(),
     transforms.Grayscale(num_output_channels=1),
-    transforms.Lambda(apply_pca_float)
     
     ])
 
 # Load and preprocess the dataset using ImageFolder
-dataset = ImageFolder(root='new_dataset', transform=transform)
+dataset_pca = ImageFolder(root=dataset_path, transform=transform)
+#dataset[0][0]
+images_np = np.array([img.flatten() for img, _ in dataset_pca])
+labels = [label for _, label in dataset_pca]
 
+pca = PCA(n_components=input_size, svd_solver='auto')
+transformed_image = pca.fit_transform(images_np)
+
+#back to tensor
+transformed_image = torch.from_numpy(transformed_image)
+dataset = [(transformed_image[i].float(), labels[i]) for i in range(len(labels))]
+print(dataset)
 
 train_set, test_set = train_test_split(dataset, test_size=0.3, random_state=42)
 test_set, val_set = train_test_split(test_set, test_size=0.5, random_state=42)
@@ -129,13 +152,13 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(input_size, neurons)  #TODO change the number of neurons, with a reference
-        self.relu = nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid()
         self.fc2 = nn.Linear(neurons, num_classes)  
 
     def forward(self, x):
         x = self.flatten(x)
         x = self.fc1(x)
-        x = self.relu(x)
+        x = self.sigmoid(x)
         x = self.fc2(x)
         return torch.softmax(x, dim=1)
 
@@ -144,9 +167,9 @@ model = MLP(input_size, num_classes)
 
 # Define the loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.01)    # TODO change optimizer - SGD, Adam, RMSprop
+#optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.01)    # TODO change optimizer - SGD, Adam, RMSprop
 #weight_decay is the coefficient for the L2 regularization term. It helps prevent overfitting by penalizing large weights.
-#optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
 
 #DATA FOR PERFORMANCE
@@ -156,7 +179,7 @@ train_acc = []
 
 
 # variables for early stopping
-n_epochs_stop = 3
+n_epochs_stop = 5
 best_valid_loss = float('inf')
 epochs_no_improve = 0
 
@@ -213,6 +236,7 @@ with torch.no_grad():
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
+        print(labels, predicted)
 
 accuracy = correct / total
 print('Test accuracy:', accuracy)
